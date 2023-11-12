cf172a941abdeb5f58ff1bf5ef7244c587813cd3
Haedam Kwon <godeka@naver.com>
2023-11-12 19:10:40 +0900

    feat: to-do 생성 초기 페이지

---
+++ src/app/createTodo/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+
+export default function() {
+    return (
+        <>
+            <h1>운동 To-do</h1>
+            <Button variant="contained">
+                운동 To-do 추가
+            </Button>
+        </>
+    );
+}
