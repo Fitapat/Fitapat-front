0404d511e97bf7dd20d33c9014ec361031f00c0f
Jiwon kang <kk3012002@gmail.com>
2023-11-12 23:00:29 +0900

    fix: selectpic, canvas 작동되게 수정

--- src/app/selectpic/page.jsx
+++ src/app/selectpic/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { Button, Box } from '@mui/material';
 import styles from './page.module.css';
