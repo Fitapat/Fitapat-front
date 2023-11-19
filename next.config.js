4a3268c8ac70e80ff5ff28d76781735df9df8478
haram8009 <haram8009@gmail.com>
2023-11-19 23:29:35 +0900

    fix: canvas 웹팩 문제 해결

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    // esmExternals: "loose",
+  },
+  webpack: (config) => {
+    config.externals.push({ canvas: 'canvas' });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
