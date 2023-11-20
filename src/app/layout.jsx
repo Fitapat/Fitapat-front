bd162d261475d74377e458b562730660f05b3cbd
Haedam Kwon <godeka@naver.com>
2023-11-20 11:21:25 +0900

    design: Container로 전체 페이지 감싸기

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Container } from '@mui/material';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Container>{children}</Container>
+      </body>
     </html>
   );
 }
