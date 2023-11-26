4b15e294e125e174dcd258aa93bbc2004abfd891
Haedam Kwon <godeka@naver.com>
2023-11-27 00:29:30 +0900

    feat: mui-Drawer 사용

--- src/app/createTodo/page.jsx
+++ src/app/createTodo/page.jsx
@@ -1,16 +1,111 @@
 'use client';
 
 import { useState } from 'react';
-import { Box, Button } from '@mui/material';
+import { Box, Button, SwipeableDrawer } from '@mui/material';
 import { Input } from '@mui/base';
+import PropTypes from 'prop-types';
+import { Global } from '@emotion/react';
+import { styled } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { grey } from '@mui/material/colors';
+import Skeleton from '@mui/material/Skeleton';
+import Typography from '@mui/material/Typography';
 
-export default function() {
-    return (
-        <Box>
-            <h1>운동 To-do</h1>
-            <Button variant="contained" fullWidth={true}>
-                운동 To-do 추가
-            </Button>
-        </Box>
-    );
+const drawerBleeding = 56;
+
+const StyledBox = styled(Box)(() => ({
+  backgroundColor: grey[300],
+}));
+
+// 서랍창 상단 작은 막대기
+const Puller = styled(Box)(({ theme }) => ({
+  width: 30,
+  height: 6,
+  backgroundColor: 'white',
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 15px)',
+}));
+
+function SwipeableEdgeDrawer(props) {
+  const { window } = props;
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  // This is used only for the example
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box>
+      <CssBaseline />
+      <Global
+        styles={{
+          '.MuiDrawer-root > .MuiPaper-root': {
+            height: `calc(90% - ${drawerBleeding}px)`,
+            overflow: 'visible',
+            width: '100%',
+            maxWidth: '430px',
+            margin: '0 auto'
+          }
+        }}
+      />
+      <Box>
+        <Typography variant="h4" sx={{ my: 2 }}>
+            운동 To-do 생성
+        </Typography>
+        <Button variant='outlined' fullWidth={true} onClick={toggleDrawer(true)}>운동 To-do 추가</Button>
+      </Box>
+      <SwipeableDrawer
+        container={container}
+        anchor="bottom"
+        open={open}
+        onClose={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        swipeAreaWidth={drawerBleeding}
+        disableSwipeToOpen={false}
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <StyledBox
+          sx={{
+            position: 'absolute',
+            top: -drawerBleeding,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            visibility: 'visible',
+            right: 0,
+            left: 0,
+          }}
+        >
+          <Puller />
+          <Typography sx={{ p: 2, color: 'text.secondary' }}>51 results</Typography>
+        </StyledBox>
+        <StyledBox
+          sx={{
+            px: 2,
+            pb: 2,
+            height: '100%',
+            overflow: 'auto',
+          }}
+        >
+          <Skeleton variant="rectangular" height="100%" />
+        </StyledBox>
+      </SwipeableDrawer>
+    </Box>
+  );
 }
+
+SwipeableEdgeDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default SwipeableEdgeDrawer;
