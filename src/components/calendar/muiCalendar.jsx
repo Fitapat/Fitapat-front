811b4cba2438434e4a856a4e473853c37ae77dab
haram8009 <haram8009@gmail.com>
2023-12-06 14:27:20 +0900

    feat: MuiCalendar 군더더기 빼기

--- src/components/calendar/muiCalendar.jsx
+++ src/components/calendar/muiCalendar.jsx
@@ -9,40 +9,12 @@ import { PickersDay } from '@mui/x-date-pickers/PickersDay';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
 
-function getRandomNumber(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-/**
- * Mimic fetch with abort controller https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
- * ⚠️ No IE11 support
- */
-function fakeFetch(date, { signal }) {
-  return new Promise((resolve, reject) => {
-    const timeout = setTimeout(() => {
-      const daysInMonth = date.daysInMonth();
-      const daysToHighlight = [1, 2, 3].map(() =>
-        getRandomNumber(1, daysInMonth),
-      );
-
-      resolve({ daysToHighlight });
-    }, 500);
-
-    signal.onabort = () => {
-      clearTimeout(timeout);
-      reject(new DOMException('aborted', 'AbortError'));
-    };
-  });
-}
-
-const initialValue = dayjs('2023-12-17');
-
 function ServerDay(props) {
   const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
 
   const isSelected =
     !props.outsideCurrentMonth &&
-    highlightedDays.indexOf(props.day.date()) >= 0;
+    highlightedDays.includes(day.format('YYYY-MM-DD'));
 
   return (
     <Badge
@@ -59,43 +31,27 @@ function ServerDay(props) {
   );
 }
 
-export default function DateCalendarServerRequest() {
-  const requestAbortController = React.useRef(null);
+export default function MuiCalendar() {
   const [isLoading, setIsLoading] = React.useState(false);
-  const [highlightedDays, setHighlightedDays] = React.useState([1, 2, 15]);
+  const [highlightedDays, setHighlightedDays] = React.useState([
+    '2023-12-17',
+    '2023-12-18',
+    '2023-12-19',
+  ]);
+
+  const initialValue = dayjs('2023-12-17');
 
   const fetchHighlightedDays = (date) => {
-    const controller = new AbortController();
-    fakeFetch(date, {
-      signal: controller.signal,
-    })
-      .then(({ daysToHighlight }) => {
-        setHighlightedDays(daysToHighlight);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        // ignore the error if it's caused by `controller.abort`
-        if (error.name !== 'AbortError') {
-          throw error;
-        }
-      });
-
-    requestAbortController.current = controller;
+    console.log(date.format('YYYY-MM-DD')); // 2023-12-01
+    setHighlightedDays(['2023-12-17', '2023-12-18', '2023-12-19']);
+    setIsLoading(false);
   };
 
   React.useEffect(() => {
     fetchHighlightedDays(initialValue);
-    // abort request on unmount
-    return () => requestAbortController.current?.abort();
   }, []);
 
   const handleMonthChange = (date) => {
-    if (requestAbortController.current) {
-      // make sure that you are aborting useless requests
-      // because it is possible to switch between months pretty quickly
-      requestAbortController.current.abort();
-    }
-
     setIsLoading(true);
     setHighlightedDays([]);
     fetchHighlightedDays(date);
