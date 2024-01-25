d7a09d3e71e8c91d118d08cf8bdc88e7e4652f28
jm3789 <jm37890@gmail.com>
2024-01-25 20:40:20 +0900

    feat: 패스워드 update api

---
+++ src/app/api/auth/reset/route.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+
+import { hash } from 'bcryptjs';
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function PUT(req) {
+  try {
+    const body = await req.json();
+
+    // 해당 이메일이 db에 있는지 체크하기
+    const existingUser = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    // 해당 이메일을 찾을 수 없음
+    if (!existingUser) {
+      return NextResponse.json({ error: '회원을 찾을 수 없습니다.' });
+    }
+
+    // 이메일을 찾은 경우: 해당 이메일의 비밀번호를 변경한다
+    const hashedPassword = await hash(body.password, 12);
+    const res = await prisma.user.update({
+      where: {
+        email: body.email,
+      },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    // 성공시 response
+    return NextResponse.json({ msg: '비밀번호 변경됨', result: res });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: '서버 에러' });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- src/app/reset/[userToken]/page.jsx
+++ src/app/reset/[userToken]/page.jsx
@@ -9,6 +9,7 @@ import { Box, Typography, Button, Stack, TextField } from '@mui/material';
 export default function Reset(p) {
   const [userEmail, setUserEmail] = useState();
   const [userPassword, setUserPassword] = useState();
+  const [confirmUserPassword, setConfirmUserPassword] = useState('');
 
   useEffect(() => {
     const verifyResetToken = async () => {
@@ -38,25 +39,35 @@ export default function Reset(p) {
     verifyResetToken(); // 렌더링될 때 한 번 실행
   }, []);
 
-  const handleResetPassword = async (e) => {
+  const handleResetSubmit = async (e) => {
     e.preventDefault();
-    alert(`이메일: ${userEmail}, 새 비밀번호: ${userPassword}`);
-    // 새 비밀번호를 db에 적용하기
-    /*
-    const res = await fetch('/api/auth/reset', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: userEmail, password: userPassword }),
-    });
-    if (res.ok) {
-      alert(`${email}님의 비밀번호가 변경되었습니다. 다시 로그인해주세요.);
+    // alert(`이메일: ${userEmail}, 새 비밀번호: ${userPassword}`);
+    if (userPassword !== confirmUserPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
     } else {
-      console.error(res.error);
-      alert('비밀번호 변경 실패');
+      try {
+        // 새 비밀번호를 db에 적용하기
+        const res = await fetch('/api/auth/reset', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email: userEmail, password: userPassword }),
+        });
+        if (res.ok) {
+          alert(
+            `${userEmail}님의 비밀번호가 변경되었습니다. 다시 로그인해주세요.`,
+          );
+          window.location = '/login';
+        } else {
+          console.error(res.error);
+          alert('비밀번호 변경 실패');
+        }
+      } catch (error) {
+        console.error(error);
+        alert('비밀번호 변경 실패');
+      }
     }
-    */
   };
 
   return (
@@ -79,7 +90,15 @@ export default function Reset(p) {
             setUserPassword(e.target.value);
           }}
         />
-        <Button variant="contained" onClick={handleResetPassword}>
+        <TextField
+          type="password"
+          label="새 비밀번호 확인"
+          variant="outlined"
+          onChange={(e) => {
+            setConfirmUserPassword(e.target.value);
+          }}
+        />
+        <Button variant="contained" onClick={handleResetSubmit}>
           설정 완료
         </Button>
       </Stack>
