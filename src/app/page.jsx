f663b0ef50cb7b2aafa52d892f8b2186639c07b2
godeka <godeka@naver.com>
2024-01-26 10:29:18 +0900

    feat: 홈에 연동

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,29 @@
+'use client';
+
+import { useState } from 'react';
 import React from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import Box from '@mui/material/Box';
 import MuiCalendar from '../components/calendar/muiCalendar';
 import TodoItem from '../components/todo/todoItem';
+import CreateTodoBtn from '../components/createTodo/createTodoBtn';
+import CreateTodoDrawer from '../components/createTodo/createTodoDrawer';
 
 export default function Home() {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggleDrawer = (newOpen) => {
+    setOpenDrawer(newOpen);
+  };
+
   return (
     <Box sx={{ height: '100%', overflowY: 'scroll' }}>
       <MuiCalendar />
       <TodoItem />
       <TodoItem />
+      <CreateTodoBtn toggleDrawer={toggleDrawer} />
+      <CreateTodoDrawer open={openDrawer} toggleDrawer={toggleDrawer} />
     </Box>
   );
 }
