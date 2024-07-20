9c41a8d4f20215fb22840715acde4ab11fc2df9f
jm3789 <jm37890@gmail.com>
2024-07-20 23:15:54 +0900

    chore: 코드 포맷팅 적용

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,7 +1,14 @@
 'use client';
 
 import React from 'react';
-import { Button, Stack, Link, Avatar, Typography, Card, CardContent } from '@mui/material';
+import {
+  Button,
+  Stack,
+  Avatar,
+  Typography,
+  Card,
+  CardContent,
+} from '@mui/material';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -14,26 +21,30 @@ export default function User() {
   const { data: session, status } = useSession();
 
   if (status === 'authenticated') {
-    const card = ( <React.Fragment><CardContent>
-      <Stack direction="column" alignItems="center" spacing={2}>      
-      <Avatar
-        alt={session.user.nickname}
-        src="/broken-image.jpg"
-        sx={{width: 80, height:80, fontSize:50}}
-      />
-      <Typography variant='h5'>
-        {session.user.nickname}
-      </Typography>
-    
-      <Button variant="text" onClick={() => signOut()} sx={{ margin: 0 }}>
-        로그아웃
-      </Button>
+    const card = (
+      <React.Fragment>
+        <CardContent>
+          <Stack direction="column" alignItems="center" spacing={2}>
+            <Avatar
+              alt={session.user.nickname}
+              src="/broken-image.jpg"
+              sx={{ width: 80, height: 80, fontSize: 50 }}
+            />
+            <Typography variant="h5">{session.user.nickname}</Typography>
 
-    </Stack></CardContent></React.Fragment>);
+            <Button variant="text" onClick={() => signOut()} sx={{ margin: 0 }}>
+              로그아웃
+            </Button>
+          </Stack>
+        </CardContent>
+      </React.Fragment>
+    );
     return (
       <div>
         <Stack spacing={4} sx={{ padding: 4 }}>
-          <Card variant='outlined' sx={{padding:1.5}}>{card}</Card>
+          <Card variant="outlined" sx={{ padding: 1.5 }}>
+            {card}
+          </Card>
           <List sx={{ width: '100%' }}>
             <ListItemButton>
               <ListItemIcon>
@@ -54,26 +65,34 @@ export default function User() {
   }
 
   return (
-    <div style={{height: '100%', display:'flex', justifyContent: 'center',
-      alignItems: 'center' }}>
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Stack direction="column" alignItems="center" spacing={2}>
-      <Typography variant='h7'>로그인 후 이용 가능한 서비스입니다.</Typography>
-      <Stack direction="row">
-      <Button
-        variant="text"
-        onClick={() => (window.location.href = '/register')}
-        sx={{ margin: 0 }}
-      >
-        회원가입
-      </Button>
-      <Button
-        variant="text"
-        onClick={() => (window.location.href = '/login')}
-        sx={{ margin: 0 }}
-      >
-        로그인
-      </Button>
-      </Stack>
+        <Typography variant="h7">
+          로그인 후 이용 가능한 서비스입니다.
+        </Typography>
+        <Stack direction="row">
+          <Button
+            variant="text"
+            onClick={() => (window.location.href = '/register')}
+            sx={{ margin: 0 }}
+          >
+            회원가입
+          </Button>
+          <Button
+            variant="text"
+            onClick={() => (window.location.href = '/login')}
+            sx={{ margin: 0 }}
+          >
+            로그인
+          </Button>
+        </Stack>
       </Stack>
     </div>
   );
