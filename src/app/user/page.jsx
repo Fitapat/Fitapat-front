8ca64f03a13b129fb669246f073b8bb455f99bd9
jm3789 <jm37890@gmail.com>
2024-07-20 23:07:33 +0900

    design: 유저 페이지 하단에 리스트 적용

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -2,6 +2,10 @@
 
 import React from 'react';
 import { Button, Stack, Link, Avatar, Typography, Card, CardContent } from '@mui/material';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import SettingsApplicationsSharpIcon from '@mui/icons-material/SettingsApplicationsSharp';
 import DisabledByDefaultRoundedIcon from '@mui/icons-material/DisabledByDefaultRounded';
 import { signIn, signOut, useSession } from 'next-auth/react';
@@ -15,7 +19,7 @@ export default function User() {
       <Avatar
         alt={session.user.nickname}
         src="/broken-image.jpg"
-        sx={{width: 100, height:100, fontSize:50}}
+        sx={{width: 80, height:80, fontSize:50}}
       />
       <Typography variant='h5'>
         {session.user.nickname}
@@ -28,23 +32,23 @@ export default function User() {
     </Stack></CardContent></React.Fragment>);
     return (
       <div>
-        <Stack spacing={6} sx={{ padding: 3 }}>
+        <Stack spacing={4} sx={{ padding: 4 }}>
           <Card variant='outlined' sx={{padding:1.5}}>{card}</Card>
-          <br/>
-          <Stack direction="row" spacing={1} alignItems="center">
-            <SettingsApplicationsSharpIcon fontSize="large" color="action"/>
-            <Link href="/" color="inherit" underline="hover">
-              설정
-            </Link>
-          </Stack>
-          <Stack direction="row" spacing={1} alignItems="center">
-            < DisabledByDefaultRoundedIcon fontSize="large" color="action"/>
-            <Link href="/delete" color="inherit" underline="hover">
-              회원탈퇴
-            </Link>
-          </Stack>
-         
-      </Stack>
+          <List sx={{ width: '100%' }}>
+            <ListItemButton>
+              <ListItemIcon>
+                <SettingsApplicationsSharpIcon />
+              </ListItemIcon>
+              <ListItemText primary="설정" />
+            </ListItemButton>
+            <ListItemButton onClick={() => (window.location.href = '/delete')}>
+              <ListItemIcon>
+                <DisabledByDefaultRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary="회원 탈퇴" />
+            </ListItemButton>
+          </List>
+        </Stack>
       </div>
     );
   }
